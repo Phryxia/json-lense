050c48245da0e87a979eec1f6b96c68aedfb0417
Phryxia <xahhaepica@gmail.com>
2024-11-10 14:31:52 +0900

    Refactor `renderJSONAsToken`

--- src/components/JSONRenderer/logic.ts
+++ src/components/JSONRenderer/logic.ts
@@ -39,88 +39,141 @@ type RenderJSONAsTokenParams = {
   idCounter: Counter
 }
 
-function* renderJSONAsToken({
+function* renderJSONAsToken(
+  params: RenderJSONAsTokenParams,
+): IterableIterator<JSONToken> {
+  const { value } = params
+  if (value === undefined) {
+    yield* renderJSONUndefinedAsToken(params)
+    return
+  }
+  if (value === null) {
+    yield* renderJSONNullAsToken(params)
+    return
+  }
+  if (typeof value === 'boolean') {
+    yield* renderJSONBooleanAsToken(params)
+    return
+  }
+  if (typeof value === 'number') {
+    yield* renderJSONNumberAsToken(params)
+    return
+  }
+  if (typeof value === 'string') {
+    yield* renderJSONStringAsToken(params)
+    return
+  }
+  if (value instanceof Array) {
+    yield* renderJSONArrayAsToken(params)
+    return
+  }
+  yield* renderJSONObjectAsToken(params)
+}
+
+function* renderJSONUndefinedAsToken({
+  idCounter,
+}: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
+  yield {
+    type: JSONTokenType.Null,
+    content: 'undefined',
+    id: idCounter.next(),
+  }
+}
+
+function* renderJSONNullAsToken({
+  idCounter,
+}: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
+  yield {
+    type: JSONTokenType.Null,
+    content: 'null',
+    id: idCounter.next(),
+  }
+}
+
+function* renderJSONBooleanAsToken({
+  value,
+  idCounter,
+}: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
+  yield {
+    type: JSONTokenType.Boolaen,
+    content: value.toString(),
+    id: idCounter.next(),
+  }
+}
+
+function* renderJSONNumberAsToken({
+  value,
+  idCounter,
+}: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
+  yield {
+    type: JSONTokenType.Number,
+    content: value.toString(),
+    id: idCounter.next(),
+  }
+}
+
+function* renderJSONStringAsToken({
+  value,
+  idCounter,
+}: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
+  yield {
+    type: JSONTokenType.String,
+    content: `"${sanitizeString(value)}"`,
+    id: idCounter.next(),
+  }
+}
+
+function* renderJSONArrayAsToken({
   value,
   tabChar,
   tabs,
   idCounter,
 }: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
-  if (value === undefined) {
+  yield {
+    type: JSONTokenType.Parenthesis,
+    content: '[',
+    id: idCounter.next(),
+  }
+  yield { type: JSONTokenType.LineBreak }
+
+  for (const child of value) {
     yield {
-      type: JSONTokenType.Null,
-      content: 'undefined',
+      type: JSONTokenType.Whitespace,
+      content: tabChar.repeat(tabs + 1),
       id: idCounter.next(),
     }
-    return
-  }
-  if (value === null) {
-    yield { type: JSONTokenType.Null, content: 'null', id: idCounter.next() }
-    return
-  }
-  if (typeof value === 'boolean') {
+    yield* renderJSONAsToken({
+      value: child,
+      tabChar,
+      tabs: tabs + 1,
+      idCounter,
+    })
     yield {
-      type: JSONTokenType.Boolaen,
-      content: value.toString(),
-      id: idCounter.next(),
-    }
-    return
-  }
-  if (typeof value === 'number') {
-    yield {
-      type: JSONTokenType.Number,
-      content: value.toString(),
-      id: idCounter.next(),
-    }
-    return
-  }
-  if (typeof value === 'string') {
-    yield {
-      type: JSONTokenType.String,
-      content: `"${sanitizeString(value)}"`,
-      id: idCounter.next(),
-    }
-    return
-  }
-  if (value instanceof Array) {
-    yield {
-      type: JSONTokenType.Parenthesis,
-      content: '[',
+      type: JSONTokenType.Delimiter,
+      content: ',',
       id: idCounter.next(),
     }
     yield { type: JSONTokenType.LineBreak }
-
-    for (const child of value) {
-      yield {
-        type: JSONTokenType.Whitespace,
-        content: tabChar.repeat(tabs + 1),
-        id: idCounter.next(),
-      }
-      yield* renderJSONAsToken({
-        value: child,
-        tabChar,
-        tabs: tabs + 1,
-        idCounter,
-      })
-      yield {
-        type: JSONTokenType.Delimiter,
-        content: ',',
-        id: idCounter.next(),
-      }
-      yield { type: JSONTokenType.LineBreak }
-    }
-
-    yield {
-      type: JSONTokenType.Whitespace,
-      content: tabChar.repeat(tabs),
-      id: idCounter.next(),
-    }
-    yield {
-      type: JSONTokenType.Parenthesis,
-      content: ']',
-      id: idCounter.next(),
-    }
-    return
   }
+
+  yield {
+    type: JSONTokenType.Whitespace,
+    content: tabChar.repeat(tabs),
+    id: idCounter.next(),
+  }
+  yield {
+    type: JSONTokenType.Parenthesis,
+    content: ']',
+    id: idCounter.next(),
+  }
+}
+
+function* renderJSONObjectAsToken({
+  value,
+  tabChar,
+  tabs,
+  idCounter,
+}: RenderJSONAsTokenParams): IterableIterator<JSONToken> {
   yield { type: JSONTokenType.Parenthesis, content: '{', id: idCounter.next() }
   yield { type: JSONTokenType.LineBreak }
 
