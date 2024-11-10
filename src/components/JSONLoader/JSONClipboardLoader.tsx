28b4eb497c6d24f95f63a71a75e90767e6beb5b0
Phryxia <xahhaepica@gmail.com>
2024-11-11 00:25:45 +0900

    Apply `JSONInspector` to `JSONClipboardLoader`

--- src/components/JSONLoader/JSONClipboardLoader.tsx
+++ src/components/JSONLoader/JSONClipboardLoader.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
+import { JSONInspector } from '../JSONInspector'
 import { JSONSyntaxError } from './JSONSyntaxError'
 import { LoaderProps } from './types'
 
 export function JSONClipboardLoader({ onLoad }: LoaderProps) {
+  const [json, setJson] = useState<any>()
   const [source, setSource] = useState('')
   const [error, setError] = useState<Error>()
 
@@ -14,22 +16,31 @@ export function JSONClipboardLoader({ onLoad }: LoaderProps) {
     try {
       const json = JSON.parse(text)
       onLoad(json)
+      setJson(json)
       setError(undefined)
     } catch (e) {
       if (e instanceof Error) {
         setError(e)
+        setJson(undefined)
       }
     }
   }
 
   return (
     <article>
-      <button onClick={handleClick}>Click to paste!</button>
+      <section>
+        <button onClick={handleClick}>Click to paste!</button>
+      </section>
 
       {error && (
         <section>
           <hr />
           <JSONSyntaxError error={error} source={source} />
+        </section>
+      )}
+      {json !== undefined && (
+        <section>
+          <JSONInspector json={json} height={200} />
         </section>
       )}
     </article>
