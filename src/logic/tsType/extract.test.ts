852ebb821bffc0a212760310080171cd75cc459f
Phryxia <xahhaepica@gmail.com>
2025-04-14 02:20:52 +0900

    Implement `extractTsType` and their test

---
+++ src/logic/tsType/extract.test.ts
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest'
+import { extractTsType } from './extract'
+
+describe('tsType/extract', () => {
+  it('should extract primitive type', () => {
+    expect(extractTsType(true)).toBe('boolean')
+    expect(extractTsType(1)).toBe('number')
+    expect(extractTsType('s')).toBe('string')
+    expect(extractTsType(null)).toBe('null')
+    expect(extractTsType(undefined)).toBe('undefined')
+  })
+
+  it('should extract array type', () => {
+    expect(extractTsType([1, 's', null])).toMatchObject([
+      'number',
+      'string',
+      'null',
+    ])
+    expect(extractTsType([[]])).toMatchObject([[]])
+  })
+
+  it('should extract object type', () => {
+    expect(extractTsType({ a: 1, b: 's', c: null })).toMatchObject({
+      a: 'number',
+      b: 'string',
+      c: 'null',
+    })
+    expect(extractTsType({ a: { b: 1 } })).toMatchObject({ a: { b: 'number' } })
+  })
+})
---
+++ src/logic/tsType/extract.ts
@@ -0,0 +1,30 @@
+import type { TsType } from '@src/model/tsType'
+import { isPrimitiveType } from './shared'
+
+export function extractTsType(value: any): TsType {
+  if (value === null) {
+    return 'null'
+  }
+
+  // BigInt won't be here
+  const typeOfValue = typeof value as TsType
+
+  if (isPrimitiveType(typeOfValue)) {
+    return typeOfValue
+  }
+
+  if (Array.isArray(value)) {
+    return value.map(extractTsType)
+  }
+
+  if (typeof value === 'object') {
+    return Object.fromEntries(
+      Object.entries(value).map(([key, childValue]) => [
+        key,
+        extractTsType(childValue),
+      ]),
+    )
+  }
+
+  throw new Error(`Unsupported type: ${typeof value}`)
+}
