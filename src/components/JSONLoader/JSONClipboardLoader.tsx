7be3879844663dcbe3574249f18b836e2c0f4bed
Phryxia <xahhaepica@gmail.com>
2025-05-07 21:39:07 +0900

    Remove redundant `JSONInspector` from `JSONClipboardLoader`

--- src/components/JSONLoader/JSONClipboardLoader.tsx
+++ src/components/JSONLoader/JSONClipboardLoader.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import { JSONInspector } from '../JSONInspector'
 import { JSONSyntaxError } from './JSONSyntaxError'
 import { LoaderProps } from './types'
 
 export function JSONClipboardLoader({ onLoad }: LoaderProps) {
-  const [json, setJson] = useState<any>()
   const [source, setSource] = useState('')
   const [error, setError] = useState<Error>()
 
@@ -16,12 +14,10 @@ export function JSONClipboardLoader({ onLoad }: LoaderProps) {
     try {
       const json = JSON.parse(text)
       onLoad(json)
-      setJson(json)
       setError(undefined)
     } catch (e) {
       if (e instanceof Error) {
         setError(e)
-        setJson(undefined)
       }
     }
   }
@@ -36,11 +32,6 @@ export function JSONClipboardLoader({ onLoad }: LoaderProps) {
         <section>
           <hr />
           <JSONSyntaxError error={error} source={source} />
-        </section>
-      )}
-      {json !== undefined && (
-        <section>
-          <JSONInspector json={json} height={200} />
         </section>
       )}
     </>
