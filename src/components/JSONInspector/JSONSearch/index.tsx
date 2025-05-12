f0612b075cd3cbf495720f1fa014317aacf7f66b
Phryxia <xahhaepica@gmail.com>
2025-05-13 03:11:17 +0900

    Implement #2 - Ask user select copying JSON only or with code
    Also refactor css by removing redundant attributes and fix nasty gaps or border mismatches

--- src/components/JSONInspector/JSONSearch/index.tsx
+++ src/components/JSONInspector/JSONSearch/index.tsx
@@ -9,6 +9,7 @@ import {
 import { Link as LinkIcon } from 'iconoir-react/regular'
 import { encodeToDeepLink } from '@src/logic/deeplink'
 import { getModel } from '@src/logic/monaco/getEditor'
+import { useOutsideClickHandler } from '@src/logic/useOutsideClickHandler'
 import { useJSONInspector } from '../JSONInspectorContext'
 import type { JSONSearchResult } from '../types'
 import { extractResult, searchFromLine } from './logic'
@@ -102,20 +103,30 @@ export function JSONSearch({}: Props) {
     }
   }
 
-  function handleCopyLink() {
+  const [isCopyPromptShown, setIsCopyPromptShown] = useState(false)
+
+  function handleCopyLink(isCodeIncluded: boolean) {
     navigator.clipboard.writeText(
       `${window.location.origin}${window.location.pathname}?data=${encodeToDeepLink(
         {
           input: json,
-          reactorCode: getModel('input')?.getValue() ?? '',
+          reactorCode: isCodeIncluded
+            ? getModel('input')?.getValue()
+            : undefined,
         },
       )}`,
     )
+    setIsCopyPromptShown(false)
   }
+
+  const copySelectorRefInitializer = useOutsideClickHandler<HTMLDivElement>(
+    () => setIsCopyPromptShown(false),
+  )
 
   return (
     <fieldset role="group" className={cx('search-bar')}>
       <input
+        className={cx('search-input')}
         type="text"
         placeholder="Enter keyword here"
         onChange={(e) => setKeyword(e.target.value)}
@@ -134,56 +145,92 @@ export function JSONSearch({}: Props) {
             />
             <span className={cx('search-count')}>/{matches.length}</span>
           </label>
-          <button
-            className={cx('search-option-button')}
-            onClick={handleMoveLeftClick}
-            disabled={matches.length === 0}
-          >
-            ◀
-          </button>
-          <button
-            className={cx('search-option-button')}
-            onClick={handleMoveRightClick}
-            disabled={matches.length === 0}
-          >
-            ▶
-          </button>
+          <div className={cx('search-option')}>
+            <button
+              className={cx('search-option-button')}
+              onClick={handleMoveLeftClick}
+              disabled={matches.length === 0}
+            >
+              ◀
+            </button>
+          </div>
+          <div className={cx('search-option')}>
+            <button
+              className={cx('search-option-button')}
+              onClick={handleMoveRightClick}
+              disabled={matches.length === 0}
+            >
+              ▶
+            </button>
+          </div>
         </>
       )}
-      <button
-        role="checkbox"
-        className={cx('search-option-button')}
-        data-tooltip="Match cases"
-        aria-checked={isMatchCase}
-        onClick={() => setIsMatchCase((flag) => !flag)}
-      >
-        Aa
-      </button>
-      <button
-        role="checkbox"
-        className={cx('search-option-button')}
-        data-tooltip="Match whole word"
-        aria-checked={isMatchWord}
-        onClick={() => setIsMatchWord((flag) => !flag)}
-      >
-        ab
-      </button>
-      <button
-        role="checkbox"
-        className={cx('search-option-button')}
-        data-tooltip="Use regular expression"
-        aria-checked={isRegexUsed}
-        onClick={() => setIsRegexUsed((flag) => !flag)}
-      >
-        (.)*
-      </button>
-      <button
-        className={cx('search-option-button', 'share')}
-        data-tooltip="Copy share link"
-        onClick={handleCopyLink}
-      >
-        <LinkIcon />
-      </button>
+      <div className={cx('search-option')}>
+        <button
+          role="checkbox"
+          className={cx('search-option-button')}
+          data-tooltip="Match cases"
+          aria-checked={isMatchCase}
+          onClick={() => setIsMatchCase((flag) => !flag)}
+        >
+          Aa
+        </button>
+      </div>
+      <div className={cx('search-option')}>
+        <button
+          role="checkbox"
+          className={cx('search-option-button')}
+          data-tooltip="Match whole word"
+          aria-checked={isMatchWord}
+          onClick={() => setIsMatchWord((flag) => !flag)}
+        >
+          ab
+        </button>
+      </div>
+      <div className={cx('search-option')}>
+        <button
+          role="checkbox"
+          className={cx('search-option-button')}
+          data-tooltip="Use regular expression"
+          aria-checked={isRegexUsed}
+          onClick={() => setIsRegexUsed((flag) => !flag)}
+        >
+          (.)*
+        </button>
+      </div>
+
+      <div className={cx('search-option')} ref={copySelectorRefInitializer}>
+        <button
+          className={cx('search-option-button', 'share')}
+          data-tooltip="Copy share link"
+          onClick={() =>
+            setIsCopyPromptShown((isCopyPromptShown) => !isCopyPromptShown)
+          }
+        >
+          <LinkIcon />
+        </button>
+
+        {isCopyPromptShown && (
+          <div className={cx('copy-selector')}>
+            <button
+              className={cx('search-option-button')}
+              onClick={() => handleCopyLink(false)}
+            >
+              JSON
+              <br />
+              Only
+            </button>
+            <button
+              className={cx('search-option-button')}
+              onClick={() => handleCopyLink(true)}
+            >
+              With
+              <br />
+              Code
+            </button>
+          </div>
+        )}
+      </div>
     </fieldset>
   )
 }
