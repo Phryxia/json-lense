7bedcee82658c3cfbde8fa0e0371e98eb7f25dae
Phryxia <xahhaepica@gmail.com>
2025-07-05 17:09:43 +0900

    Add jsdoc to properties of `JSONSearchResult`

--- src/components/JSONInspector/types.ts
+++ src/components/JSONInspector/types.ts
@@ -1,6 +1,10 @@
 export type JSONSearchResult = {
+  /** Line number starts from 0 where search target exists */
   lineIndex: number
+  /** Multiple keyword might be exists in one line */
   tokenId: number
+  /** Character position starts from 0 where search target starts */
   beginPosInToken: number
+  /** Character position where search target ends */
   endPosInToken: number
 }
